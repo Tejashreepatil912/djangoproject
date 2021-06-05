9c0d8b297072c383ad803178978f6d646fdc0b2b
django.project <tejashree.patil1996@gmail.com>
2021-06-05 12:15:16 +0530

    second commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,15 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+STATIC_ROOT= BASR_DIR / 'static'
 
 
 #manually added
-
-
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,"static"),
-]
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+#STATICFILES_DIRS=[
+#os.path.join(BASE_DIR,"static"),
+#Activate Django-Heroku.
+#django_heroku.settings(locals())
