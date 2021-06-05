d829b1c3b8acec30d60a0bea62943705db8fc419
django.project <tejashree.patil1996@gmail.com>
2021-06-05 12:09:07 +0530

    second commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn projectname.wsgi --log-file -
+web: waitress-serve --port=$PORT django_project.wsgi:application
--- django_project/settings.py
+++ django_project/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u6s90+-a!nqihf5-b+=c!bxqj6uw=7hgy_b@bj&7$%a!=wcr9('
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://biryanizone.herokuapp.com/"]
+ALLOWED_HOSTS = ["biryanizone.herokuapp.com","localhost"]
 
 
 # Application definition
